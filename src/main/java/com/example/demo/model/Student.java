d91207536056826b61011df6635532f8674e2a6b
RAJARAM RANE <60419018+rajaramrane@users.noreply.github.com>
2024-05-07 22:31:54 +0530

    Update Student.java for javax use

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -1,61 +1,78 @@
 package com.example.demo.model;
 
-import java.time.LocalDate;
+import java.sql.Date;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-@Table(name="student_table")
-@Data
+@Table(name = "student_table")
 public class Student {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer id;
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Integer getClassRoom() {
-		return classRoom;
-	}
-	public void setClassRoom(Integer classRoom) {
-		this.classRoom = classRoom;
-	}
-	public LocalDate getDateOfBirth() {
-		return dateOfBirth;
-	}
-	public void setDateOfBirth(LocalDate dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getEmail_ID() {
-		return email_ID;
-	}
-	public void setEmail_ID(String email_ID) {
-		this.email_ID = email_ID;
-	}
-	private String name;
-	private Integer classRoom;
-	private LocalDate dateOfBirth;
-	private String address;
-	private String email_ID;
-	 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String  name;
+    private Integer classRoom;
+    
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss", timezone = "Asia/Kolkata")
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "date_of_birth")	
+    private Date    dateOfBirth;
+    private String  address;
+    private String  emailID;
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getClassRoom() {
+        return classRoom;
+    }
+
+    public void setClassRoom(Integer classRoom) {
+        this.classRoom = classRoom;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail_ID() {
+        return emailID;
+    }
+
+    public void setEmail_ID(String emailID) {
+        this.emailID = emailID;
+    }
+
 }
+
